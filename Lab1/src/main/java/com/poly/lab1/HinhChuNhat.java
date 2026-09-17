d57a0cf74d8262e6a297f2e8c5139539e40031c2
admin <tranxuanhung8455@gmail.com>
2026-09-17 13:20:53 +0700

    Hoan thanh Lab1 Bai 4

---
+++ Lab1/src/main/java/com/poly/lab1/HinhChuNhat.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.lab1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tranx
+ */
+public class HinhChuNhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cd, cr, cv, dt;
+        System.out.printf("Nhap chieu dai: ");
+        cd = sc.nextInt();
+        System.out.printf("Nhap chieu rong: ");
+        cr = sc.nextInt();
+        cv = (cd + cr) * 2;
+        dt = cd * cr;
+        System.out.printf("Chu vi hinh chu nhat: %d\nDien tich hinh chu nhat: %d", cv, dt);
+    }
+}
